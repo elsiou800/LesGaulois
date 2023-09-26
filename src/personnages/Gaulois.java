24cc739f93df523a735983aed3661b3958f24403
Elsa Ayache <chl4304a@ens.fsi>
2023-09-26 19:59:44 +0200

    fin TP1.2

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,6 +3,7 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
+	private int effetPotion = 1;
 	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -20,7 +21,24 @@ public class Gaulois {
 	}
 
 	private String prendreParole() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Le gaulois " + nom + ":";
+	}
+	public void frapper(Romain romain) {
+		System.out.println(nom +  "envoie un grand coup dans la machoire de " + romain.getNom());
+		romain.recevoirCoup(force/3);
+		
+	}
+
+	
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	public static void main(String[] args) {
+		Gaulois Asterix = new Gaulois ("Asterix", 8);
+		System.out.println(Asterix);
+		System.out.println(Asterix.prendreParole() + "Je suis asterix le gaulois");
+		System.out.println(Asterix.parler("je suis asterix"));
+		
+		
 	}
 }	
